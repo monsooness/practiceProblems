bb6ad2fc9f121320a483c8c45b2183a9b93a6e89
Monsoon <nepzsoon@gmail.com>
2018-10-12 00:50:21 -0700

    Improce encoder-decoder

--- encoder-decoder.js
+++ encoder-decoder.js
@@ -9,46 +9,46 @@
 //Output: aaaaaabbbbbbbbbbb
 
 function encoder(str) {
- let arr = [];
- let count = 1;
-
- for (var i=0; i<str.length; i++) {
-   if (str[i] === str[i+1]) {
-     count ++
-   } else {
-     arr.push(str[i-1],count)
-     count += 1
-   }
+  let arr = [];
+  let count = 1;
+ 
+  for (var i=0; i<str.length-1; i++) {
+    if (str[i] === str[i+1]) {
+      count ++
+      
+    } else {
+      arr.push(str[i-1],count)
+      count = 1
+    }
+  }
+   arr.push(str[str.length-1],count)
+  return arr;
  }
- return arr;
+
+
+
+ function decoder(str) {
+  let nums = ['1','2','3','4','5','6','7','8','9','0']
+  let result = ''
+  let letter = ''
+ 
+  for (var i=0; i<str.length; i++) {
+    var numbers = ''
+    if (nums.includes(str[i])) {
+    
+      while(i<str.length && nums.includes(str[i])) {
+        numbers += str[i]
+         i++
+      }
+      i--
+      for (var j=0; j<numbers; j++) {
+       result += letter
+      }
+    } else {
+      letter = str[i]
+      console.log(letter)
+    }
+
+  }
+  console.log(result)
 }
-
-
-
-function decoder(str) {
- let nums = ['1','2','3','4','5','6','7','8','9','0']
- let result = ''
- let letter = ''
- for (var i=0; i<str.length; i++) {
-   console.log(i, 'hi')
-   var numbers = ''
-   if (nums.includes(str[i])) {
-  
-     while(i<str.length && nums.includes(str[i])) {
-       numbers += str[i]
-        i++
-     }
-     i--
-     for (var j=0; j<numbers; j++) {
-      result += letter
-     }
-   } else {
-     letter = str[i]
-     console.log(letter)
-   }
-
- }
- console.log(result)
-}
-
-decoder('a20b11')
