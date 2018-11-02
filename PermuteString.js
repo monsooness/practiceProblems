c9ae2192687bbc629c9b1a5dab0ba1c58d4da849
Monsoon <nepzsoon@gmail.com>
2018-11-01 21:07:03 -0700

    Implement permutation

--- PermuteString.js
+++ PermuteString.js
@@ -1,38 +1,19 @@
-//Using backtracking -- time complexity is N! (i think)
-
-var permArr = [], usedChars = [];
-function permute(input) {
-  //convert input into a char array (one element for each character)
-  var i, ch, chars = input.split("");
-  for (i = 0; i < chars.length; i++) {
-    //get and remove character at index "i" from char array
-    ch = chars.splice(i, 1);
-    //add removed character to the end of used characters
-    usedChars.push(ch);
-    //when there are no more characters left in char array to add, add used chars to list of permutations
-    if (chars.length == 0) permArr[permArr.length] = usedChars.join("");
-    //send characters (minus the removed one from above) from char array to be permuted
-    permute(chars.join(""));
-    //add removed character back into char array in original position
-    chars.splice(i, 0, ch);
-    //remove the last character used off the end of used characters array
-    usedChars.pop();
+var permute = function(nums) {
+  let results = [];
+  
+  if (nums.length === 1) {
+      results.push(nums)
+      return results
   }
+  
+  for (let i=0; i<nums.length; i++) {
+      let first = nums[i];
+      let left = nums.slice(0,i) + nums.slice(i+1)
+      let innerPermutation = permute(left)
+      
+      for (let j=0; j<innerPermutation.length; j++) {
+          results.push(first + innerPermutation[j])
+      }
+  }
+  return results
 };
-
-function permute(nums) {
-  let res = [];
-
-  function find(curr, rest) {
-    if (!rest.length) return res.push(curr);
-
-    for (let i = 0; i < rest.length; i++) {
-      find(
-      [...curr, rest[i]],[...rest.slice(0, i), ...rest.slice(i + 1)]);
-    }
-  }
-
-  find([], nums);
-
-  return res;
-}
