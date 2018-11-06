532086106fcfdfac0259780d02cda61b38eb1030
Monsoon <nepzsoon@gmail.com>
2018-11-05 23:43:53 -0800

    Implement quetion from today's interview

---
+++ str1_occurences.js
@@ -0,0 +1,7 @@
+// Given two strings S1 and S2, at each step you are allowed to choose two characters c1 and c2, and replace 
+// all occurrences of c1 in S1 to c2. What is the minimum number of steps required to transform S1 to S2?
+
+
+
+
+// aabb → bbaa
