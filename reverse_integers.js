dc23ab820c72b56e139e6db98add3ca8dcafd294
Monsoon <nepzsoon@gmail.com>
2018-11-18 17:24:42 -0800

    Implement reverse 32 bit integer

---
+++ reverse_integers.js
@@ -0,0 +1,22 @@
+Given a 32-bit signed integer, reverse digits of an integer.
+
+function reverse(strNum) {
+    let startingInd = 0;
+    let result = '';
+  
+    if(strNum[0] == '-') {
+      console.log(7)
+      result += '-'
+      startingInd++
+    } else if (strNum[0] == '0') {
+      while (strNum[startingInd] == '0' && startingInd<strNum.length) {
+        startingInd++
+      }
+    }
+    for (let i=strNum.length-1; i>=startingInd; i--) {
+      result += strNum[i]
+    }
+    return result
+  }
+  
+  reverse('000123')
