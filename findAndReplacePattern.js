d9117c368a7707b69e5c02b7e2a76a25f3032584
Monsoon <nepzsoon@gmail.com>
2018-11-30 14:38:32 -0800

    Implement bug fix

--- findAndReplacePattern.js
+++ findAndReplacePattern.js
@@ -7,28 +7,29 @@
 
 var findAndReplacePattern = function(words, pattern) { 
 
-    function patternMapping(word, pattern) {
-      let obj = {};
-      if (word.length !== pattern.length) {
-        return false
-      } else {
-        for (let i=0; i<word.length; i++) {
-          if (Object.keys(obj).indexOf(word[i]) > -1 && obj[word[i]]!== pattern[i]) {
-            return false        
-          } else {
-            obj[word[i]] = pattern[i]
-          }
+  function patternMapping(word, pattern) {
+    let obj = {};
+    let obj2 = {};
+    if (word.length !== pattern.length) {
+      return false
+    } else {
+      for (let i=0; i<word.length; i++) {
+        if (Object.keys(obj).indexOf(word[i]) > -1 && obj[word[i]]!== pattern[i]) {
+          return false  
+        } else if (Object.keys(obj2).indexOf(pattern[i]) > -1 && obj2[pattern[i]]!== word[i]) {
+          return false
+        } else {
+          obj[word[i]] = pattern[i]
+          obj2[pattern[i]] = word[i]
         }
-        return true
       }
+      return true
     }
-  
-    patternMapping('deq', 'aqq')
-    
-    let result = words.filter( word => {
-      return patternMapping(word, pattern)
-    })
-    return result
-  };
-  
+  }
+
+  let result = words.filter( word => {
+    return patternMapping(word, pattern)
+  })
+  return result
+};
   findAndReplacePattern(["dkd","deq","mee","aqq","dkd","ccc"],'abb')
